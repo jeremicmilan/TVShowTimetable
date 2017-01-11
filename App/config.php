aba042f41946323f025f2b538e1f115143d4dc0d
jeremicmilan <milanjeremic@live.com>
2017-01-11 11:45:36 +0000

    Added composer (now using composer autoload) and added aura/session package

--- App/config.php
+++ App/config.php
@@ -1,19 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pveb_student
- * Date: 14/12/16
- * Time: 12:22
- */
 
 namespace App;
 
 class Config
 {
-
     const site_name='TV Shows Timetable';
 
-    const default_controller='pages';
+    const default_controller='timetable';
     const default_action='index';
 
     const DB_HOST="localhost";
