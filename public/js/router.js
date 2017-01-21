368b172fdfea0256eef60a29037d70acb493cede
jeremicmilan <milanjeremic@live.com>
2017-01-21 10:58:40 +0000

    Added support for params

--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,29 @@
-function redirect(controller, action = "index")
+function redirect(controller, action = "index", params = [])
 {
     var path = window.location.pathname;
     var app_name = "TVShowsTimetable";
+    var paramsString = "";
+
+    for (let index = 0; index < params.length; ++index)
+    {
+        if (index == 0)
+        {
+            paramsString = params[index];
+        }
+        else
+        {
+            paramsString = paramsString + "/" + params[index];
+        }
+    }
 
     path = path.substr(0, path.search(app_name) + app_name.length);
 
-    window.location = "http://"+window.location.hostname + path + "/" + controller + "/" + action;
+    var uri = "http://"+window.location.hostname + path + "/" + controller + "/" + action;
+
+    if (params.length > 0)
+    {
+        uri += "/" + paramsString;
+    }
+
+    window.location = uri;
 }
