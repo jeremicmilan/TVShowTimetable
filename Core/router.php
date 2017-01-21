78a6d7cf0ccdbf7f1af5b5758aa6994b7cc7d040
jeremicmilan <milanjeremic@live.com>
2017-01-21 10:58:28 +0000

    Fixed bugs in the router - throwing an error for no controller - params not filled with undefined even though is empty

--- Core/router.php
+++ Core/router.php
@@ -56,27 +56,29 @@ class Router {
         $path = $uri_parts[0];
         $path_parts = explode('/', $path);
 
-        $i = 0;
         $length = count($path_parts);
 
-        if($length > 1) {
-
+        if ($length > 0 && $path_parts[0] != "")
+        {
             // Get controller - first element of array
-            if($i < $length) {
-                $this->controller = strtolower($path_parts[$i]);
-                $i++;
-            }
-
-            // Get action - next element of array
-            if($i < $length) {
-                $this->action = preg_replace_callback(  '/_([a-z])/',
-                                                        function ($matches) { return strtoupper($matches[1]); },
-                                                        strtolower($path_parts[$i]));
-                $i++;
-            }
-
-            // Get params - the rest of elements in array
-            $this->params = array_slice($path_parts, $i);
+            $this->controller = strtolower($path_parts[0]);
         }
+
+        if ($length > 1)
+        {
+            // Get action - next element of array
+            $this->action = preg_replace_callback('/_([a-z])/',
+                                                  function ($matches)
+                                                  {
+                                                      return strtoupper($matches[1]);
+                                                  },
+                                                  strtolower($path_parts[1]));
+        }
+
+        // Get params - the rest of elements in array
+        if ($length > 2)
+            $this->params = array_slice($path_parts, 2);
+        else
+            $this->params = [];
     }
 }
