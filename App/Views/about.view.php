2cdad8316321db9f47a59c1b2f818fd66a0f2d0d
jeremicmilan <milanjeremic@live.com>
2017-01-22 22:52:32 +0000

    Added about view

---
+++ App/Views/about.view.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>TVShows About</title>
+
+        <?php include "include/stylesheets.php" ?>
+        <?php include "include/scripts.php" ?>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <?php include 'include/nav.php' ?>
+
+        <div class="container">
+            <h1 style="align-content: center">TV Shows Timetable About</h1>
+            <br/>
+            <p>This is a website page made by Jana Protic, Milan Jeremic and Milos Mitrovic in order of showing newly
+                made skills in web programming.</p>
+            <p>On <b>TVShowsTimetable</b> </p>
+            <p>On <b>Shows</b> page you may see all the TV shows in the database or if you are logged in you
+                may see all the shows you are following.</p>
+        </div>
+    </body>
+</html>
